3d1f26e2f44b50a41ae6e17e87bf838dde742b25
resol341 <resol341@gmail.com>
2025-01-08 16:24:48 +0000

    Deploying to gh-pages from @ SizhaoLu/sizhaolu.github.io@755ad88b186c394e9e4ecb9d28528bdc65865f1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-sizhao-lu-research-profile",
-    title: "Sizhao Lu Research Profile",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
